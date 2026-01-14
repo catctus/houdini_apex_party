07e116793d61bd2f358553cd525c8565cf6f6505
Anton Stattin <anton.stattin@gmail.com>
2026-01-14 08:04:38 +0100

    Update insert_controls.py
    reworked with guides instead as apex intended (as far as I know lol)

--- insert_controls.py
+++ insert_controls.py
@@ -13,75 +13,98 @@ rigname, graph = character.getRig(graph_name=rigname, graph=graph, bypass=bypass
 ##### header end #####
 
 
-def add_extra_controls(graph: ApexGraphHandle, 
-                       #guides: Geometry,
-                       target: String,
-                       trf_pos: Int,
-                       after_prefix: String,
-                       before_prefix: String,
-                       promote_t: Bool,
-                       promote_r: Bool,
-                       promote_s: Bool):
+def addExtraControls(graph: ApexGraphHandle, 
+                     guides: Geometry,
+                     setups: DictArray) -> tuple[ApexGraphHandle, Geometry]:
     
-    # gather data
-    target_node = graph.findNode(target)
-    parent_node = graph.GetTransformParent(target_node)
-    children_node_array = graph.GetTransformChildren(target_node)
+    for setup in setups:
     
-    new_controls : ApexNodeIDArray = []
-    # if we building an after control (or both)
-    if trf_pos == 0 or trf_pos==2:
-        beforeCtrl = graph.addNode(f"{target}{after_prefix}", callback="TransformObject")
-        if promote_t:
-            beforeCtrl.t_in.promote(f"{target}{after_prefix}_t")
-            beforeCtrl.r_in.promote(f"{target}{after_prefix}_r")
-            beforeCtrl.s_in.promote(f"{target}{after_prefix}_s")
+        target : String = setup["TargetControlName#"]
+        trf_pos : Int = setup["NewTransformPosition#"]
+        after_prefix : String = setup["PrefixAfterControl#"]
+        before_prefix : String = setup["PrefixBeforeControl#"]
+        promote_t : Bool = setup["PromoteT#"]
+        promote_r : Bool = setup["PromoteR#"]
+        promote_s : Bool = setup["PromoteS#"]
         
-        target_node.xform_out.connect(beforeCtrl.parent_in)
+        # gather data
+        target_node = graph.findNode(target)
+        parent_node = graph.GetTransformParent(target_node)
+        children_node_array : ApexNodeIDArray = graph.GetTransformChildren(target_node)
         
-        new_controls.append(beforeCtrl)
-        
-    graph.AddSetPointTransforms("pointtransform", new_controls, srcport="xform")
+        new_controls : ApexNodeIDArray = []
+        # if we building an after control (or both)
+        if trf_pos == 0 or trf_pos==2:
+            pos = guides.getPointTransform(name=target)
+            _, driverpt, drivername = guides.findOrAddGuide(name=f"{target}{after_prefix}",
+                                  xform=pos,
+                                  parent=target,
+                                  promote="t r s",
+                                  shapescale=(10,10,10),
+                                  shape="cross_wires",
+                                  overridecontrol=True,
+                                  setshapedata=True,
+                                  )
+            guides.updateJoint(ptnum=driverpt, name=drivername, xform=pos)
+            
+            pattern = f'@name={drivername}'
+            #parentname = NodeFromPattern(driverparent)
+            #guides.SetParent(pattern, driverpt)
+            guides = graph.controlsFromGuides(geo=guides, group=pattern)
+            
+            #guides.AddSetPointTransforms(name=drivername)
+            
+            graph.layout()
+            
+            
+        """
+            beforeCtrl = graph.addNode(f"{target}{after_prefix}", callback="TransformObject")
+            #graph.UpdateNodeParms(beforeCtrl, {"properties":{shapescale:(10,10,10)}})
+            if promote_t:
+                beforeCtrl.t_in.promote(f"{target}{after_prefix}_t")
+            if promote_r:
+                beforeCtrl.r_in.promote(f"{target}{after_prefix}_r")
+            if promote_s:
+                beforeCtrl.s_in.promote(f"{target}{after_prefix}_s")
+            
+            target_node.xform_out.connect(beforeCtrl.parent_in)
+            
+            for child_node in children_node_array:
+                beforeCtrl.xform_out.connect(child_node.parent_in)
+                target_node.localxform_out.connect(child_node.parentlocal_in)
+                
+            
+            new_controls.append(beforeCtrl)
+        """ 
+        #graph.AddSetPointTransforms("pointtransform", new_controls, srcport="xform")
     
+    graph.layout()
     
-    
-    return graph
+    return graph, guides
 
 # create a bind multiparam
-add_controls = bindMultiparm(preset_kwargs={"label":"test"})
+setups = bindMultiparm(preset_kwargs={"label":"test"})
 
-addToMultiparm(add_controls, "TargetControlName", "")
-addToMultiparm(add_controls, "NewTransformPosition", 0, 
+addToMultiparm(setups, "TargetControlName", "")
+addToMultiparm(setups, "NewTransformPosition", 0, 
                preset='menu',
                preset_kwargs={'menu_labels':['after', 'before', 'both'],
                               'menu_items':[0,1,2]})
-addToMultiparm(add_controls, "PrefixAfterControl", "_secondary",preset_kwargs={'joins_with_next':1})
-addToMultiparm(add_controls, "PrefixBeforeControl", "_offset", preset_kwargs={"disable_when":"{NewTransformPosition==2}"})
+addToMultiparm(setups, "PrefixAfterControl", "_secondary",preset_kwargs={'joins_with_next':1})
+addToMultiparm(setups, "PrefixBeforeControl", "_offset", preset_kwargs={"disable_when":"{NewTransformPosition==2}"})
 
 # could maybe split this up as well.. 
-addToMultiparm(add_controls, "PromoteT", True, preset_kwargs={'joins_with_next':1})
-addToMultiparm(add_controls, "PromoteR", True, preset_kwargs={'joins_with_next':1})
-addToMultiparm(add_controls, "PromoteS", False)
+addToMultiparm(setups, "PromoteT", True, preset_kwargs={'joins_with_next':1})
+addToMultiparm(setups, "PromoteR", True, preset_kwargs={'joins_with_next':1})
+addToMultiparm(setups, "PromoteS", False)
 
-for ctrl in add_controls:
-    target_ctrl_name : String = ctrl["TargetControlName#"]
-    trf_pos : Int = ctrl["NewTransformPosition#"]
-    after_prefix : String = ctrl["PrefixAfterControl#"]
-    before_prefix : String = ctrl["PrefixBeforeControl#"]
-    promote_t : Bool = ctrl["PromoteT#"]
-    promote_r : Bool = ctrl["PromoteR#"]
-    promote_s : Bool = ctrl["PromoteS#"]
-    
-    if not bypass:
-        graph = add_extra_controls(graph, 
-                                    #guides,
-                               target_ctrl_name,
-                               trf_pos,
-                               after_prefix, 
-                               before_prefix,
-                               promote_t,
-                               promote_r,
-                               promote_s)
+guidesource: String = bindInput()
+
+#Setup
+guides = character.findCharacterElement(guidesource)
+guides = graph.addExtraControls(guides=guides, setups=setups)
+character.updateCharacterElement(guidesource, guides)
+
 
 
 
