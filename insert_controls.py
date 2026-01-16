6f70cf9a1d0d9a2f34f9c138c429d388a10f02ad
Anton Stattin <anton.stattin@gmail.com>
2026-01-16 17:44:38 +0100

    Update insert_controls.py

--- insert_controls.py
+++ insert_controls.py
@@ -12,31 +12,31 @@ bypass: Bool = BindInput()
 rigname, graph = character.getRig(graph_name=rigname, graph=graph, bypass=bypass)
 ##### header end #####
 # we want to make this into a subgraph
-# we want to make this into a subgraph
-def AddControl(graph:ApexGraphHandle, 
-               guides:Geometry,
-               name:String,
-               match:String="",
-               parent:String="",
-               promote:String ="t r s", 
-               shapescale:Vector3 = Vector3(1,1,1),
-               shape:String = "cross_wires",
-               shapeColor:Vector3 = Vector3(0,1,0),
-               shapeTranslate:Vector3 = Vector3(0,0,0),
-               shapeRotate:Vector3 = Vector3(0,0,0),
-               shapeScale: Vector3 = Vector3(1,1,1),
-               translateOffset:Vector3 = Vector3(0,0,0),
-               rotateOffset:Vector3 = Vector3(0,0,0),
-               scaleOffset:Vector3 = Vector3(1,1,1),
-               offsetMode:Int = 0,
-               rotateOrder:Int = 0,
-               scaleInheritance:Int=0,
-               transformOrder:Int = 0,
-               overrideControl:Bool = True,
-               setShapeData:Bool = True,
-               tags:StringArray=[])->tuple[ApexGraphHandle, Geometry, ApexNodeID]:
+def AddControl(graph: ApexGraphHandle, 
+               guides: Geometry,
+               name: String,
+               match: String = "",
+               parent: String = "",
+               promote: String = "t r s", 
+               shapescale: Vector3 = Vector3(1,1,1),
+               shape: String = "cross_wires",
+               shapeColor: Vector3 = Vector3(0,1,0),
+               shapeTranslate: Vector3 = Vector3(0,0,0),
+               shapeRotate: Vector3 = Vector3(0,0,0),
+               shapeScale: Vector3 = Vector3(10,10,10),
+               translateOffset: Vector3 = Vector3(0,0,0),
+               rotateOffset: Vector3 = Vector3(0,0,0),
+               scaleOffset: Vector3 = Vector3(1,1,1),
+               offsetMode: Int = 0,
+               rotateOrder: Int = 0,
+               scaleInheritance: Int=0,
+               transformOrder: Int = 0,
+               overrideControl: Bool = True,
+               setShapeData: Bool = True,
+               tags: StringArray=[])->tuple[ApexGraphHandle, Geometry, ApexNodeID]:
     
     matchname = rkutil.FirstNodeNameFromPattern(graph, match)
+    
     
     if matchname == "":
         matchxform:Matrix4 = value(); 
@@ -71,8 +71,117 @@ def AddControl(graph:ApexGraphHandle,
     control : ApexNodeID = nodeids[0]
     
     return graph, guides, control
-                  
 
+def AddConstraint(graph : ApexGraphHandle,
+                  guides : Geometry,
+                  driver: String,
+                  driven: String,
+                  components: Int = 7,
+                  ignoreoffset: Bool = False,
+                  parentConstraint: Bool = True
+                  ):
+                  
+    driverName = rkutil.FirstNodeNameFromPattern(graph, driver)
+    drivenName = rkutil.FirstNodeNameFromPattern(graph, driven)
+    
+    driverNode = graph.FindNode(driverName)
+    drivenNode = graph.FindNode(drivenName)    
+    
+    constraintNode = drivenNode
+    # this will keep the transform intact
+    if parentConstraint:
+        reset: Matrix4 = value()
+        drivenJnt = guides.findJoint(drivenName)
+        originalNodeParentPt = guides.getParent(drivenJnt)
+        originalNodeParentName = guides.jointData(originalNodeParentPt)
+        
+        
+        constraintName = f"{driverName}_{drivenName}_parentConstraint"
+        
+        # add a parent transform
+        guides, constraintNode = graph.AddControl(guides, 
+                                           constraintName,
+                                           match=drivenName,
+                                           promote="", 
+                                           parent=originalNodeParentName)
+                                                                        
+        constraintNode.xform_out.connect(drivenNode.parent_in)
+        constraintNode.localxform_out.connect(drivenNode.parentlocal_in)
+        drivenNode.updateNode(parms={'restlocal':reset})
+    
+    driverXForm = guides.getPointTransform(driverName)
+    drivenXForm = guides.getPointTransform(drivenName)
+    ingnore_offset_xform = twoWaySwitch(drivenXForm, driverXForm, ignoreoffset)
+    
+    parentblend_parms = {'newparent_bind':driverXForm, 
+                         'parent_bind':ingnore_offset_xform, 'parent':ingnore_offset_xform,
+                         'blend':1.0, 'components':components}
+                         
+    transformBlendName = f'{driverName}_{drivenName}_TransformDriverBlend'
+    parentBlend = graph.addOrUpdateNode(name=transformBlendName, callback='rig::ParentBlend',
+                                        parms=parentblend_parms)
+    parentBlend.parent_out.connect(constraintNode.xform_in)
+    driverNode.xform_out.connect(parentBlend.newparent_in)
+
+
+
+    return graph, guides
+"""
+# constraint
+def AddConstraints(graph: ApexGraphHandle, 
+                   guides: Geometry, 
+                   drivernode: ApexNodeID, 
+                   drivername: String, 
+                   driven: String, 
+                   components: Int,  
+                   ingnoreoffset: Bool, 
+                   drivenoffsetctrls: Bool,
+                   parentconstaint: Bool):
+                   
+        
+        nodes = graph.findNodes(f'{driven} - {drivername}')
+        driverxform = guides.getPointTransform(drivername)
+        
+        for node in nodes:
+            
+            drivenname = node.name()
+            
+            if drivenoffsetctrls:
+                guides, drivenname, drivernode, driverxform = graph.AddConstraintControls(guides=guides, name=drivenname, 
+                                                                                          driver=drivername, node=node, 
+                                                                                          promote=True, parent=drivername)
+            
+            if parentconstaint:
+                original_node = node
+                reset: Matrix4 = value()
+                drivenjnt = guides.findJoint(drivenname)
+                originalnodeparentpt = guides.getParent(drivenjnt)
+                originalnodeparentname = guides.jointData(originalnodeparentpt)
+                
+                guides, drivenname, node = graph.AddConstraintControls(guides=guides, name=drivenname, 
+                                                                       driver=drivername, node=node, 
+                                                                       promote=False, parent=originalnodeparentname)
+                node.xform_out.connect(original_node.parent_in)
+                node.localxform_out.connect(original_node.parentlocal_in)
+                original_node.updateNode(parms={'restlocal':reset})
+                
+                
+            drivenxform = guides.getPointTransform(drivenname)
+            ingnore_offset_xform = twoWaySwitch(drivenxform, driverxform, ingnoreoffset)
+            
+            parentblend_parms = {'newparent_bind':driverxform, 
+                                 'parent_bind':ingnore_offset_xform, 'parent':ingnore_offset_xform,
+                                 'blend':1.0, 'components':components}
+            drivenname = f'{drivenname}_TransformDriverBlend'
+            parentblend = graph.addOrUpdateNode(name=drivenname, callback='rig::ParentBlend',
+                                                parms=parentblend_parms)
+            parentblend.parent_out.connect(node.xform_in)
+            drivernode.xform_out.connect(parentblend.newparent_in)
+
+
+
+        return graph, guides
+"""
 def BuildControl(graph:ApexGraphHandle, 
                  guides:Geometry, 
                  guideTarget:ApexNodeID, 
@@ -95,38 +204,15 @@ def BuildControl(graph:ApexGraphHandle,
     
     # create the main control
     guides, mainctr = graph.AddControl(guides, f"{name}_{controlSuffix}", match=guidename, parent=parent)
-    driver=mainctr
+    driver : String =mainctr.name()
     
     # create the secondary controls
     if buildSecondaryCtr:
         guides, secctr = graph.AddControl(guides, f"{name}_{secondarySuffix}_{controlSuffix}", match=guidename, parent=mainctr.name())        
-        driver=secctr
+        driver=secctr.name()
+        
+    guides = graph.AddConstraint(guides, driver, guidename)
     
-    """
-    ctrlxform, ctrlpt, ctrname = guides.findOrAddGuide(name=name,
-                                                       xform=pos,
-                                                       parent=guideparentname,
-                                                       promote="t r s",
-                                                       shapescale=(10,10,10),
-                                                       shape="cross_wires",
-                                                       overridecontrol=True,
-                                                       setshapedata=True,
-                                                      )
-    guides.updateJoint(ptnum=ctrlpt, name=ctrname, xform=pos)
-    
-    pattern = f"@name={ctrname}"
-    guides, nodeids = graph.controlsFromGuides(geo=guides, group=pattern)
-    
-    ctrlNode : ApexNodeID = nodeids[0]
-    
-    ctrlNode.xform_out.connect(guideTarget.parent_in)
-    ctrlNode.localxform_out.connect(guideTarget.parentlocal_in)
-    
-    reset : Matrix4 = value()
-    
-    guideTarget.updateNode(parms={'restlocal':reset})
-    """
-
     return graph, guides
 
     
@@ -186,8 +272,9 @@ addToMultiparm(setups, "buildSecondary", True, preset_kwargs={'joins_with_next':
 addToMultiparm(setups, "buildOffset", False, preset_kwargs={'joins_with_next':1})
 
 addToMultiparm(setups, "controlShape", "cross_wires", preset_kwargs={'joins_with_next':1})
+addToMultiparm(setups, "secondaryControlShape", "cross_wires", preset_kwargs={'joins_with_next':1})
 addToMultiparm(setups, "offsetControlShape", "cross_wires", preset_kwargs={'joins_with_next':1})
-addToMultiparm(setups, "parentControlShape", "cross_wires", preset_kwargs={'joins_with_next':1})
+# might have to fix this one
 addToMultiparm(setups, "_shapeReference", "cross_wires", preset='controlshapes')
 
 addToFolder("ControlSetup", ["guideTarget#", "useGuideTargetParent#", "controlParent#", "useGuideName#", 
